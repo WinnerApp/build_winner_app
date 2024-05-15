3632a64bbaa83193e4b03845518a81b890d84f7c
josercc <josercc@163.com>
2024-05-15 14:22:17 +0800

    🔴[Fix] 降低发布版本为2.3.0

--- bin/build_winner_app.dart
+++ bin/build_winner_app.dart
@@ -1,27 +1,29 @@
+import 'package:args/command_runner.dart';
+import 'package:build_winner_app/commands/build/build_command.dart';
 import 'package:build_winner_app/commands/send_text_to_webhooks.dart';
-import 'package:dart_ops_engine/commons/dart_ops_engine.dart';
+// import 'package:dart_ops_engine/commons/dart_ops_engine.dart';
 
-// void main(List<String> arguments) async {
-//   var arguments0 = [...arguments];
-//   for (var arg in arguments) {
-//     if (arg.startsWith('--dart-define=')) {
-//       dartDefineArgs.add(arg);
-//       arguments0.remove(arg);
-//     }
-//   }
+void main(List<String> arguments) async {
+  var arguments0 = [...arguments];
+  for (var arg in arguments) {
+    if (arg.startsWith('--dart-define=')) {
+      dartDefineArgs.add(arg);
+      arguments0.remove(arg);
+    }
+  }
 
-//   final runner = CommandRunner(
-//     'build_winner_app',
-//     '打包并上传到Testflight/到蒲公英 企业微信通知',
-//   )
-//     ..addCommand(BuildCommand())
-//     ..addCommand(SendTextToWebHooksCommand());
+  final runner = CommandRunner(
+    'build_winner_app',
+    '打包并上传到Testflight/到蒲公英 企业微信通知',
+  )
+    ..addCommand(BuildCommand())
+    ..addCommand(SendTextToWebHooksCommand());
 
-//   await runner.run(arguments0);
-// }
-
-Future<void> main(List<String> args) async {
-  DartOpsEngine('build_winner_app', args)
-    ..addAction('sendTextToWebHooks', SendTextToWebHooksAction())
-    ..run();
+  await runner.run(arguments0);
 }
+
+// Future<void> main(List<String> args) async {
+//   DartOpsEngine('build_winner_app', args)
+//     ..addAction('sendTextToWebHooks', SendTextToWebHooksAction())
+//     ..run();
+// }
--- lib/commands/send_text_to_webhooks.dart
+++ lib/commands/send_text_to_webhooks.dart
@@ -4,7 +4,7 @@ import 'dart:io';
 import 'package:args/command_runner.dart';
 import 'package:build_winner_app/common/define.dart';
 import 'package:color_logger/color_logger.dart';
-import 'package:dart_ops_engine/dart_ops_engine.dart' hide logger;
+// import 'package:dart_ops_engine/dart_ops_engine.dart' hide logger;
 
 class SendTextToWebHooksCommand extends Command {
   SendTextToWebHooksCommand() {
@@ -30,16 +30,16 @@ class SendTextToWebHooksCommand extends Command {
   }
 }
 
-class SendTextToWebHooksAction extends ActionRun {
-  @override
-  Future<Map> run(Env env, Map request) async {
-    final url = request['url'];
-    final text = request['text'];
-    if (url == null || text == null) {
-      logger.log('url 或 text 未配置', status: LogStatus.error);
-      exit(1);
-    }
-    await sendTextToWeixinWebhooks(url, text);
-    return {};
-  }
-}
+// class SendTextToWebHooksAction extends ActionRun {
+//   @override
+//   Future<Map> run(Env env, Map request) async {
+//     final url = request['url'];
+//     final text = request['text'];
+//     if (url == null || text == null) {
+//       logger.log('url 或 text 未配置', status: LogStatus.error);
+//       exit(1);
+//     }
+//     await sendTextToWeixinWebhooks(url, text);
+//     return {};
+//   }
+// }
