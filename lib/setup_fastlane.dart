c738582dab8cdb0f6da19e07209b50efcd784a0e
josercc <josercc@163.com>
2024-10-22 08:43:25 +0800

    [Change] 修改了iOS fastlane的配置

--- lib/setup_fastlane.dart
+++ lib/setup_fastlane.dart
@@ -54,7 +54,7 @@ lane :upload_testflight do |options|
     changelog: changelog,
     skip_waiting_for_build_processing: false,
     distribute_external: true,
-    groups: ["内部测试公开组"],
+    groups: ["TEST1"],
     reject_build_waiting_for_review: true,
   )
 end
