5cf4f8e4b4ccc19f365532b21822955660e2d847
josercc <josercc@163.com>
2024-12-18 14:57:29 +0800

    [Remove] 停止追踪build_winner_app_test.dart

--- test/build_winner_app_test.dart
+++ test/build_winner_app_test.dart
@@ -1,5 +1,13 @@
+import 'package:build_winner_app/git_reset.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('test', () async {});
+  test(
+    'test',
+    () async {
+      await gitReset(
+          '/Users/king/Documents/winner-docs/meta-winner-app/unity/meta_winner_unity_android');
+    },
+    timeout: Timeout.none,
+  );
 }
